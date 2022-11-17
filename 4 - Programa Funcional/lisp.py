900a5bb161d36c97b1c4c46787db8d0a9beb66a1
matheusvivasr <vivas.matheus@usp.br>
2022-11-17 20:03:44 -0300

    otimizações de algumas funções

--- 4 - Programa Funcional/lisp.py
+++ 4 - Programa Funcional/lisp.py
@@ -1,17 +1,11 @@
-def fatorial(n):
-    n = int(n)
-    if n == 1:
-        return n
-    else:
-        return n*fatorial(n-1)
+def fatorial(n:int):    
+    if n<=1:return 1
+    else:return n*fatorial(n-1)
 
-def fibonacci(n):
-    n = int(n)
-    if n <= 1:
-        return 1
-    else:
-        return fibonacci(n-1)+fibonacci(n-2)
-
+def fibonacci(n:int):  
+    if n<1:return 0
+    elif n==1:return 1
+    else:return fibonacci(n-1)+fibonacci(n-2)
 
 texto = [linhas.strip().split() for linhas in open("./input.dat").readlines()]
 
